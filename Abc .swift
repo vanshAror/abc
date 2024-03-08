bfc38fd8d7386230fee099fc1b0d8dc0d608faa3
vansh <avansh2001@gmail.com>
2024-03-08 13:30:57 +0530

    Initial commit

---
+++ Abc .swift
@@ -0,0 +1,13 @@
+//
+//  Abc .swift
+//  Abc
+//
+//  Created by Mobile on 08/03/24.
+//
+
+import Foundation
+
+
+public func add (a:Int,b:Int) -> Int {
+    return a + b
+}
---
+++ Abc/Abc.h
@@ -0,0 +1,18 @@
+//
+//  Abc.h
+//  Abc
+//
+//  Created by Mobile on 08/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Abc.
+FOUNDATION_EXPORT double AbcVersionNumber;
+
+//! Project version string for Abc.
+FOUNDATION_EXPORT const unsigned char AbcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Abc/PublicHeader.h>
+
+
